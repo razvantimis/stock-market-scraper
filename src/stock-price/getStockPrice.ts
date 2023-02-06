e6d9ff3f14d9e58e99a68ebacfa69dbc1f3411c0
Razvan Timis <r@timis.dev>
2023-02-06 08:33:37 +0000

    feat: support us stocks

--- src/stock-price/getStockPrice.ts
+++ src/stock-price/getStockPrice.ts
@@ -1,8 +1,10 @@
 import type { StockPrice } from "./types";
 import type { WatchAsset } from "../types";
-import { getStockPriceFromBVBPage } from "./getStockPriceFromBVBPage.ts";
+  import { getStockPriceFromBVBPage } from "./getStockPriceFromBVBPage.ts";
+import yahooFinance from "https://esm.sh/yahoo-finance2@2.3.10"
 
-async function getLastBvbStockPrice(
+
+async function getLastROStockPrice(
   assetList: WatchAsset[]
 ): Promise<StockPrice[]> {
   const stockPriceList: StockPrice[] = [];
@@ -18,16 +20,60 @@ async function getLastBvbStockPrice(
   }
   return stockPriceList;
 }
+
+async function getLastUSStockPrice(
+  assetList: WatchAsset[]
+): Promise<StockPrice[]> {
+  const stockPriceList: StockPrice[] = [];
+
+  const symbols = assetList.map(asset => asset.symbol);
+
+  const results = await yahooFinance.quote(symbols, {
+      fields: [
+        'regularMarketOpen',
+        'regularMarketDayLow',
+        'regularMarketDayHigh',
+        'regularMarketPrice',
+        'regularMarketVolume',
+        'regularMarketTime',
+        'postMarketPrice',
+        'preMarketPrice'
+      ], return: "object"
+    });
+    
+  for (const asset of assetList) {
+    const stockData = results[asset.symbol];
+    if(!stockData){
+      console.warn("Missing stock price for " + asset.symbol)
+      continue;
+    }
+    const price = stockData.regularMarketPrice;
+    stockPriceList.push({
+      symbol: asset.symbol,
+      price: price,
+      exchangeCountryCode: asset.exchangeCountryCode,
+    });
+  }
+  return stockPriceList;
+}
+
 async function getStockPrice(watchList: WatchAsset[]): Promise<StockPrice[]> {
-  const bvbWatchList = watchList.filter(
+  const roWatchList = watchList.filter(
     (asset) => asset.exchangeCountryCode === "RO"
   );
-  console.time("Fetching BVB prices...")
-  const bvbStockPriceList = await getLastBvbStockPrice(bvbWatchList);
-  console.timeEnd("Fetching BVB prices...")
+  console.time("Fetching stock prices from RO...");
+  const roStockPriceList = await getLastROStockPrice(roWatchList);
+  console.timeEnd("Fetching stock prices from RO...");
 
-  const finalStockPriceList = [...bvbStockPriceList].sort((asset1, asset2) =>
-    asset1.symbol.localeCompare(asset2.symbol)
+  const usWatchList = watchList.filter(
+    (asset) => asset.exchangeCountryCode === "US"
+  );
+  console.time("Fetching stock prices from US...");
+  const usStockPriceList = await getLastUSStockPrice(usWatchList);
+  console.timeEnd("Fetching stock prices from US...");
+
+  const finalStockPriceList = [...roStockPriceList, ...usStockPriceList].sort(
+    (asset1, asset2) => asset1.symbol.localeCompare(asset2.symbol)
   );
 
   return finalStockPriceList;
